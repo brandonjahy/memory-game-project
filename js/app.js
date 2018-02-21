d2f61cd79d2fa29d0b3506fad3e22c9c1771b76b
Brandon-Johnson <b.johnss97@gmail.com>
2018-02-21 10:19:28 -0800

    function to make cards clickable

--- js/app.js
+++ js/app.js
@@ -1,18 +1,13 @@
-/*
- * Create a list that holds all of your cards
- */
 
+// an array that holds all the cards
+let card = document.getElementsByClassName("card");
+let cards = [...card]
+console.log(cards);
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(cards) {
-    var currentIndex = cards.length, temporaryValue, randomIndex;
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -26,23 +21,7 @@ function shuffle(cards) {
 }
 
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
-
-// an array that holds all the cards LOL
-let card = document.getElementsByClassName("card");
-let cards = [...card]
-console.log(cards);
-
+// function that will introduce the neccesary class to the clicked card
 var showCard = function (){
    this.classList.toggle("open");
    this.classList.toggle("show");
